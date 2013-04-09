154da04a56b6bc80e12a4ae43e4171b92663af97
Jen Joslin <jmj9210@gmail.com>
2013-04-09 09:51:27 -0600

    Login error handling

--- src/edu/byu/isys413/data/views/LoginView.java
+++ src/edu/byu/isys413/data/views/LoginView.java
@@ -1,31 +1,28 @@
 package edu.byu.isys413.data.views;
 
-import java.util.Hashtable;
-
-import javax.naming.Context;
-import javax.naming.NamingException;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.InitialDirContext;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 import edu.byu.isys413.data.models.BusinessObjectDAO;
 import edu.byu.isys413.data.models.DataException;
 import edu.byu.isys413.data.models.Employee;
+import edu.byu.isys413.data.models.LDAP;
 import edu.byu.isys413.data.models.SearchCriteria;
 
 public class LoginView extends Shell {
 	private Text txtNetid;
 	private Text txtPassword;
+	private int tryCount = 1;
+	private Label errorLabel;
 
 	/**
 	 * Launch the application. Only used for testing.
@@ -70,6 +67,11 @@ public class LoginView extends Shell {
 		txtPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		new Label(this, SWT.NONE);
 		
+		errorLabel = new Label(this, SWT.NONE);
+		errorLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		errorLabel.setText("");
+		new Label(this, SWT.NONE);
+		
 		Button btnLogIn = new Button(this, SWT.NONE);
 		btnLogIn.addMouseListener(new MouseAdapter() {
 			@SuppressWarnings("unchecked")
@@ -77,30 +79,39 @@ public class LoginView extends Shell {
 			public void mouseUp(MouseEvent e) {
 				String netId = txtNetid.getText();
 				String passwd = txtPassword.getText();
-				try {
-		            @SuppressWarnings("rawtypes")
-					Hashtable env = new Hashtable();
-		            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-		            env.put(Context.PROVIDER_URL, "ldaps://ldap.byu.edu/");
-		            env.put(Context.SECURITY_AUTHENTICATION, "simple");
-		            env.put(Context.SECURITY_PRINCIPAL, "uid=" + netId + ", ou=People, o=byu.edu");
-		            env.put(Context.SECURITY_CREDENTIALS, passwd);
-		            @SuppressWarnings("unused")
-					DirContext ctx = new InitialDirContext(env);
-		            Employee emp = BusinessObjectDAO.getInstance().searchForBO("Employee", new SearchCriteria("netid", netId));
-		            AppData.getInstance().setLoggedIn(true);
+				
+				LDAP ldap = new LDAP();
+				if(ldap.authenticate(netId, passwd)){
+					Employee emp = null;
+					try {
+						emp = BusinessObjectDAO.getInstance().searchForBO("Employee", new SearchCriteria("netid", netId));
+					} catch (DataException ex) {
+						ex.printStackTrace();
+					}
+					AppData.getInstance().setLoggedIn(true);
 		            AppData.getInstance().setEmployee(emp);
 		            dispose();
-		        } catch (NamingException e1) {
-		            // TODO: extra: display some feedback to the user.
-		        } catch (DataException e1) {
-		        	// TODO: extra: display some feedback to the user.
+				}
+				else{
+					
+					if(tryCount <3){
+						errorLabel.setText(("Incorrect username and/or password. Please try again.")); 
+						tryCount++;
+					}
+					else{
+						MessageBox messageBox = new MessageBox(getShell(), SWT.ICON_ERROR); 
+						messageBox.setMessage("Login failed. Please try again later."); 
+						messageBox.open(); 
+						dispose();
+					}
 				}
 				
 			}
 		});
 		btnLogIn.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		btnLogIn.setText("Log In");
+		
+		
 		createContents();
 	}
 	
@@ -109,7 +120,7 @@ public class LoginView extends Shell {
 	 */
 	protected void createContents() {
 		setText("Log In");
-		setSize(325, 135);
+		setSize(425, 135);
 
 	}
 
